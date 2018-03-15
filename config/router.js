0b01e73d809932e3bcfe462ed31ca02f3545b14d
Katie Moore <kemoore1431@gmail.com>
2018-03-15 15:16:36 +0000

    added user routes to router

--- config/router.js
+++ config/router.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
 const events = require('../controllers/events');
-// const users = require('../controllers/users');
+const users = require('../controllers/users');
 // const secureRoute = require('../lib/secureRoute');
 
+router.route('/users')
+  .new(users.new);
+
+router.route('/users/:id')
+  .get(users.show)
+  .get(users.edit)
+  .put(users.update)
+  .delete(users.delete);
+
+//Event Routes
 router.route('/events/new')
   .get(events.new);
 
