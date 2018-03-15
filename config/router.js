26c4a2f348d105c8f0f2c17fb33b30b836eb7382
Sui <suii_shum@hotmail.com>
2018-03-15 16:00:51 +0000

    added users controller

--- config/router.js
+++ config/router.js
@@ -1,26 +1,26 @@
 const router = require('express').Router();
 const events = require('../controllers/events');
 const users = require('../controllers/users');
-const secureRoute = require('../lib/secureRoute');
+// const secureRoute = require('../lib/secureRoute');
 
 router.route('/users')
-  .new(users.new);
+  // .new(users.new);
 
 router.route('/users/:id')
-  .get(secureRoute, users.show)
-  .get(secureRoute, users.edit)
-  .put(secureRoute, users.update)
-  .delete(secureRoute, users.delete);
+  .get(users.show)
+  // .get(users.edit)
+  .put(users.update)
+  .delete(users.delete);
 
 //Event Routes
 router.route('/events/new')
-  .get(secureRoute, events.new);
+  .get(events.new);
 
 router.route('/events/:id')
-  .get(secureRoute,events.show)
-  .get(secureRoute,events.edit)
-  .put(secureRoute, events.update)
-  .delete(secureRoute, events.delete);
+  .get(events.show)
+  .get(events.edit)
+  .put(events.update)
+  .delete(events.delete);
 
 router.route('/*')
   .all((req, res) => res.status(404).json({ message: 'Not found' }));
