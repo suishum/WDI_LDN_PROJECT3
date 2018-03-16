b44ea70edb596bd084cd0d2f5f890d1fc3419105
Katie Moore <kemoore1431@gmail.com>
2018-03-16 12:27:09 +0000

    made api request for restaurant drop down on new form

--- src/app.js
+++ src/app.js
@@ -3,18 +3,18 @@ import '@uirouter/angularjs';
 import 'satellizer';
 
 import Router from './config/router';
-import Auth from './config/auth';
+// import Auth from './config/auth';
 
 //
-// import EventsNewCtrl from './controllers/events/new';
+import EventsNewCtrl from './controllers/events/new';
 //
-// import Event from './services/events';
+import Event from './services/events';
 
 import 'bulma';
 import './assets/scss/style.scss';
 
 angular.module('project3', ['ui.router'])
-  .config(Router);
-  .config(Auth);
+  .config(Router)
+  // .config(Auth)
   .controller('EventsNewCtrl', EventsNewCtrl)
   .service('Event', Event);
--- src/controllers/events/new.js
+++ src/controllers/events/new.js
@@ -1,8 +1,19 @@
-EventsNewCtrl.$inject = ['Event', '$state'];
+EventsNewCtrl.$inject = ['Event', '$state', '$http'];
 
-function EventsNewCtrl(Event, $state) {
+function EventsNewCtrl(Event, $state, $http) {
   this.event = {};
+  this.restaurants = [];
 
+  $http({
+    method: 'GET',
+    url: 'https://developers.zomato.com/api/v2.1/search?entity_id=61&entity_type=city&start=0&count=99', headers: {
+      'user-key': 'bb553f8ef2af47b55acff60d10239333'
+    }
+  })
+    .then(res => {
+      this.restaurants = res.data.restaurants;
+      console.log(this.restaurants);
+    });
 
   function handleSubmit(){
     if (this.form.invalid) return false;
