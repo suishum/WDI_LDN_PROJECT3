3233b293a78829bd5affc3ec3a20677f339f1d21
Katie Moore <kemoore1431@gmail.com>
2018-03-16 14:22:12 +0000

    restaurant selections change based on users chosen location

--- src/controllers/events/new.js
+++ src/controllers/events/new.js
@@ -1,27 +1,50 @@
-EventsNewCtrl.$inject = ['Event', '$state', '$http'];
+EventsNewCtrl.$inject = ['Event', '$state', '$http', '$scope'];
 
-function EventsNewCtrl(Event, $state, $http) {
-  this.event = {};
-  this.restaurants = [];
-
-  $http({
-    method: 'GET',
-    url: 'https://developers.zomato.com/api/v2.1/search?entity_id=61&entity_type=city&start=0&count=99', headers: {
-      'user-key': 'bb553f8ef2af47b55acff60d10239333'
-    }
-  })
-    .then(res => {
-      this.restaurants = res.data.restaurants;
-      console.log(this.restaurants);
-    });
+function EventsNewCtrl(Event, $state, $http, $scope) {
+  const vm = this;
+  vm.event = {
+    restaurants: []
+  };
+  vm.restaurants = [];
 
   function handleSubmit(){
-    if (this.form.invalid) return false;
-    Event.create(this.event)
-      .then(event => $state.go('eventsShow', { id: event._id }));
+    if (vm.form.invalid) return false;
+    Event.create(vm.event)
+      .then(() => $state.go('/'));
   }
 
-  this.handleSubmit = handleSubmit;
+  function cityCode(){
+    switch (vm.event.location) {
+      case 'London':
+        vm.cityCode = 61;
+        break;
+      case 'New York':
+        vm.cityCode = 280;
+        break;
+      case 'Milan':
+        vm.cityCode = 258;
+    }
+    $http({
+      method: 'GET',
+      url: 'https://developers.zomato.com/api/v2.1/search?',
+      params: {
+        entity_id: vm.cityCode,
+        entity_type: 'city',
+        start: 0,
+        count: 100
+      },
+      headers: {
+        'user-key': 'bb553f8ef2af47b55acff60d10239333'
+      }
+    })
+      .then(res => {
+        vm.restaurants = res.data.restaurants;
+      });
+  }
+
+  vm.handleSubmit = handleSubmit;
+
+  $scope.$watch(() => vm.event.location, cityCode);
 }
 
 export default EventsNewCtrl;
