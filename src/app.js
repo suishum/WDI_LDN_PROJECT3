79d73168be0db2a2ab94585282728ac1935e05bc
Katie Moore <kemoore1431@gmail.com>
2018-03-16 10:37:59 +0000

    set up app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
+import angular from 'angular';
+import '@uirouter/angularjs';
+import 'satellizer';
 
+import Router from './config/router';
+import Auth from './config/auth';
+
+import 'bulma';
+import './assets/scss/style.scss';
+
+angular.module('project3', ['ui.router', 'satellizer'])
+  .config(Router)
+  .config(Auth);
