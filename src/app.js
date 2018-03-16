823407c1a83f6828ec66d13b6720ee18f528ec67
Sui <suii_shum@hotmail.com>
2018-03-16 15:24:14 +0000

    added src/views/auth/register and src/controllers/auth/register

--- src/app.js
+++ src/app.js
@@ -4,10 +4,14 @@ import 'satellizer';
 
 import Router from './config/router';
 import Auth from './config/auth';
+import AuthRegisterCtrl from './controllers/auth/register';
+// import AuthLoginCtrl from './controllers/auth/login';
 
 import 'bulma';
 import './assets/scss/style.scss';
 
 angular.module('project3', ['ui.router', 'satellizer'])
   .config(Router)
-  .config(Auth);
+  .config(Auth)
+  .controller('AuthRegisterCtrl', AuthRegisterCtrl);
+  // .controller('AuthLoginCtrl', AuthLoginCtrl);
