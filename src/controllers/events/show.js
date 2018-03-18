23fed99713b40bc40aee4fa3292f19936c132c30
Katie Moore <kemoore1431@gmail.com>
2018-03-18 12:28:06 +0000

    restricted invite list to users not already invited

--- src/controllers/events/show.js
+++ src/controllers/events/show.js
@@ -4,29 +4,36 @@ function EventsShowCtrl($http, Event, $state, User){
   const vm = this;
   vm.event = {};
   vm.users = [];
-  vm.selected = [];
 
   Event.findById($state.params.id)
     .then(res => {
       vm.event = res.data;
-    });
+    })
+    .then(() => updateInviteList());
 
-  User.find()
-    .then(res => {
-      vm.users = res.data;
-    });
+
+  function updateInviteList() {
+    User.find()
+      .then(res => {
+        const filtered = res.data.filter(user => vm.event.attendees.findIndex(obj => obj._id === user._id) === -1 );
+        vm.users = filtered;
+      });
+  }
 
   function invite(){
     for (let i = 0; i < vm.selected.value.length; i++) {
       vm.event.attendees.push(vm.selected.value[i]);
     }
     Event.update(vm.event);
+    vm.selected.value = [];
+    updateInviteList();
   }
 
   function removeAttendee(id){
     const i = vm.event.attendees.findIndex(obj => obj._id === id);
     vm.event.attendees.splice(i, 1);
     Event.update(vm.event);
+    updateInviteList();
   }
 
   this.invite = invite;
