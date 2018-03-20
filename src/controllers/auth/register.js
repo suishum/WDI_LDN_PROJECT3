89a1462f1c33eddc9bbdae03fe6e70bc106378aa
Katie Moore <kemoore1431@gmail.com>
2018-03-20 13:43:32 +0000

    changed form error handling

--- src/controllers/auth/register.js
+++ src/controllers/auth/register.js
@@ -21,6 +21,8 @@ function AuthRegisterCtrl($auth, $state, $http, $scope) {
   vm.restaurants = [];
 
   function handleSubmit() {
+    if(this.form.$invalid) return false;
+
     // uses satellizer to sign up
     $auth.signup(vm.user)
     // satellizer doesnt give the user a token immediately after registering (without extra logic), therefore we will redirect to the login page for now and get them to log in so we can drop a token in.
