a0fb3a8f22227e80febc496f09756653ce988025
Katie Moore <kemoore1431@gmail.com>
2018-03-22 10:29:30 +0000

    adding center marker

--- src/directives/google-map.js
+++ src/directives/google-map.js
@@ -23,17 +23,19 @@ function googleMap() {
       });
       $scope.$watch('center', () => {
         map.setCenter($scope.center);
+        const marker = new google.maps.Marker({
+          position: $scope.center,
+          map: map,
+          icon: '⭐️'
+        });
+
+        marker.setMap(map);
       }, true);
 
-      // const image = {
-      //   url: '/assets/images/ga-beer-logo.png', // url
-      //   scaledSize: new google.maps.Size(25, 25), // scaled size
-      //   origin: new google.maps.Point(0,0) // origin
-      //   // anchor: new google.maps.Point(0, 0) // anchor
-      // };
 
       const directionsService = new google.maps.DirectionsService();
-      const directionsDisplay = new google.maps.DirectionsRenderer({ suppressMarkers: true});
+      const directionsDisplay = new google.maps.DirectionsRenderer();
+      // const directionsDisplay = new google.maps.DirectionsRenderer({ suppressMarkers: true});
       directionsDisplay.setMap(map);
 
       $scope.$watch('center', () => map.setCenter($scope.center), true);
@@ -41,8 +43,12 @@ function googleMap() {
 
       navigator.geolocation.getCurrentPosition(pos => {
         currentLocation = { lat: pos.coords.latitude, lng: pos.coords.longitude };
+
         displayRoute();
       });
+
+
+
 
       // DISPLAY ROUTE
       function displayRoute() {
